77a88e81cfe25fea87142360e64156ae01b151a2
jeraldine hinestroza <60895269+jeraldineHinestrozaP@users.noreply.github.com>
2024-01-24 08:54:05 -0500

    commit 3 rama2 para el reto

---
+++ src/main/java/Children.java
@@ -0,0 +1,23 @@
+public class Children {
+    private int sons;
+    private String name_sons;
+    private int age_sons;
+
+    public Children(int sons, String name_sons, int age_sons) {
+        this.sons = sons;
+        this.name_sons = name_sons;
+        this.age_sons = age_sons;
+    }
+
+    public int getSons() {
+        return sons;
+    }
+
+    public String getName_sons() {
+        return name_sons;
+    }
+
+    public int getAge_sons() {
+        return age_sons;
+    }
+}
